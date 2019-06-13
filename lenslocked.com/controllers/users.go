7212cd7cb2f3661dbe3dad3249238743bcdd2364
barmstrong9 <brandon.armstrong@thingco.com>
2019-06-13 14:53:40 +0100

    Completed 7.4.3 in the book

--- lenslocked.com/controllers/users.go
+++ lenslocked.com/controllers/users.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 	"github.com/barmstrong9/WebDevelopmentWithGo/lenslocked.com/views"
 )
@@ -15,8 +16,16 @@ type Users struct{
 	NewView *views.View
 }
 
-func (u *Users) New(w http.ResponseWriter, r http.Request) {
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	if err := u.NewView.Render(w, nil); err != nil {
 		panic(err)
 	}
+}
+
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil{
+		panic(err)
+	}
+	fmt.Fprintln(w, r.PostForm["email"])
+	fmt.Fprintln(w, r.PostForm["password"])
 }
